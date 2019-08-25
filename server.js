f24e6b3a33cfbbe3da5e636b497d263cb016cadb
Boris Ermakov-Spektor <b.ermakovspektor@ufl.edu>
2019-08-25 16:34:10 -0400

    Finish Bootcamp1

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-var http = require('http'), 
-    fs = require('fs'), 
-    url = require('url'),
-    port = 8080;
+var http = require('http'),
+  fs = require('fs'),
+  url = require('url'),
+  port = 8080;
 
 /* Global variables */
 var listingData, server;
 
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
+  if (parsedUrl.path == '/listings') {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.write(listingData);
+    response.end();
+  } else {
+    response.writeHead(404);
+    response.end('Bad gateway error');
+    response.end();
+  }
 
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
@@ -37,15 +46,17 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
     HINT: Read up on JSON parsing Node.js
    */
 
-    //Check for errors
-  
-
-   //Save the sate in the listingData variable already defined
-  
-
+  //Check for errors
+  if (err) {
+    console.log('[JSON Reader] Error Occurred!: ' + err);
+    throw err;
+  }
+  //Save the sate in the listingData variable already defined
+  listingData = data;
   //Creates the server
-  
+  server = http.createServer(requestHandler);
   //Start the server
-
-
+  server.listen(port, function() {
+    console.log('Server listening on: http://127.0.0.1:' + port);
+  });
 });
